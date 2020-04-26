fb250a6784bf8cdf948a2e219181a8d0622454fc
Vale-polito <pro.vpereira@gmail.com>
2020-04-26 17:27:50 +0200

    :label: Started structure
    basic structure in place

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    nbItem: 6,
+  };
+  render() {
+    return this.itemRender();
+  }
+
+  itemRender = () => {
+    return (
+      <div>
+        <input type="checkbox" value="elem1" />
+        <span> 1st element </span>
+        <input type="checkbox" value="elem1" />
+        <span> 2nd element </span>
+        <input type="checkbox" value="elem1" />
+        <span> 3rd element </span>
+        <input type="checkbox" value="elem1" />
+        <span> 4th element </span>
+        <input type="checkbox" value="elem1" />
+        <span> 5th element </span>
+      </div>
+    );
+  };
 }
 
 export default App;
